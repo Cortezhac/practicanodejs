6e43f2be887eb5903399443c95735dd15a2ae2c8
Irvin Cortez <irvincotez67@gmail.com>
2020-12-14 19:56:55 -0600

    Renderizar datos de prueba

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,6 +1,7 @@
+import {post} from '../models/posts.js';
 export const renderBlog = (resquest, response) => {
   //response.sendFile(process.cwd() + "/views/blog.html");
-  response.render("blog.ejs", {path : "Blog"});
+  response.render("blog.ejs", {path : "Blog", post : post});
 }
 
-export default {renderBlog};
+export default { renderBlog };
---
+++ models/posts.js
@@ -0,0 +1,9 @@
+export const post = [{
+  title : "Mi primer post",
+  time : "Dec 09, 2020",
+  body : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis ducimus tenetur id ipsa eligendi eaque voluptatem consequatur suscipit, dicta iste, sed, cupiditate eveniet ipsam libero voluptatum quas molestiae nostrum cumque!"
+},{
+  title : "Mi segundo post" ,
+  time : "Dec 07, 2020" ,
+  body : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias unde culpa iure quidem magnam repudiandae, voluptatum delectus dolor perferendis quibusdam ipsa quasi. Nihil nam porro quis est illum cupiditate. Recusandae."
+}]
